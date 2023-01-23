d744c8f1159079f60959adfe4bf9206fb57d65a6
Jio0728 <kimjio0728@yonsei.ac.kr>
2023-01-23 14:32:09 +0900

    setup.py requirements.txt revised

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ import os
 import sys
 
 PRJ_DIR = os.path.dirname(__file__)
-print(PRJ_DIR)
 sys.path.append(PRJ_DIR)
 
-os.system('pip install -r requirements.txt')
 os.system('pip install ftfy regex tqdm gdown')
+os.system('pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html')
+os.system('pip install Ninja')
+print("install requirements.txt start")
+os.system('pip install -r requirements.txt')
+print("install requirements.txt finished")
